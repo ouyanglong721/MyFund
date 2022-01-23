3822d612e52ab00ecc2c8c9ee79aa6ed8c2a4e8b
oylong <ouyanglong721@gmail.com>
2022-01-23 18:35:04 +0800

    bugfix

--- src/com/oylong/myfund/entity/SysMenu.java
+++ src/com/oylong/myfund/entity/SysMenu.java
@@ -1,11 +1,10 @@
 package com.oylong.myfund.entity;
 
-import com.oylong.common.core.web.domain.BaseEntity;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +13,7 @@ import java.util.List;
  *
  * @author OyLong
  */
-public class SysMenu extends BaseEntity {
+public class SysMenu  {
     private static final long serialVersionUID = 1L;
 
     /**
@@ -105,8 +104,6 @@ public class SysMenu extends BaseEntity {
         this.menuId = menuId;
     }
 
-    @NotBlank(message = "菜单名称不能为空")
-    @Size(min = 0, max = 50, message = "菜单名称长度不能超过50个字符")
     public String getMenuName() {
         return menuName;
     }
@@ -131,7 +128,6 @@ public class SysMenu extends BaseEntity {
         this.parentId = parentId;
     }
 
-    @NotBlank(message = "显示顺序不能为空")
     public String getOrderNum() {
         return orderNum;
     }
@@ -140,7 +136,7 @@ public class SysMenu extends BaseEntity {
         this.orderNum = orderNum;
     }
 
-    @Size(min = 0, max = 200, message = "路由地址不能超过200个字符")
+
     public String getPath() {
         return path;
     }
@@ -149,7 +145,7 @@ public class SysMenu extends BaseEntity {
         this.path = path;
     }
 
-    @Size(min = 0, max = 200, message = "组件路径不能超过255个字符")
+
     public String getComponent() {
         return component;
     }
@@ -182,7 +178,7 @@ public class SysMenu extends BaseEntity {
         this.isCache = isCache;
     }
 
-    @NotBlank(message = "菜单类型不能为空")
+
     public String getMenuType() {
         return menuType;
     }
@@ -207,7 +203,7 @@ public class SysMenu extends BaseEntity {
         this.status = status;
     }
 
-    @Size(min = 0, max = 100, message = "权限标识长度不能超过100个字符")
+
     public String getPerms() {
         return perms;
     }
@@ -248,11 +244,6 @@ public class SysMenu extends BaseEntity {
                 .append("status ", getStatus())
                 .append("perms", getPerms())
                 .append("icon", getIcon())
-                .append("createBy", getCreateBy())
-                .append("createTime", getCreateTime())
-                .append("updateBy", getUpdateBy())
-                .append("updateTime", getUpdateTime())
-                .append("remark", getRemark())
                 .toString();
     }
 }
